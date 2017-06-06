46a5876edd6cafcbe996c503bd57fadad4516fc3
Christoph Holtmann <christoph.holtmann@live.de>
2017-06-06 16:36:19 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/TestButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class TestButton : NetworkBehaviour
+{
+    public Transform ball;
+
+    public void Click()
+    {
+        Debug.Log("button clicked");
+
+        NetworkServer.Spawn(Instantiate(ball, null).gameObject);
+
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    [Command]
+    void CmdClick()
+    {
+        Click();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
