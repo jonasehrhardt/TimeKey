e38034504353abcf091950520097b01be3821602
Jonas <jonas.ehrhardt@student.htw-berlin.de>
2017-06-19 21:03:47 +0200

    Camera Script refined

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,26 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CameraController : MonoBehaviour {
-
-    public GameObject follow = null;
-    private float dist = 0.0f;
+public class CameraController : MonoBehaviour
+{
+    public GameObject follow;
+    private float xOffset = 0.0f;
+    private float yPosition, zPosition;
 
 	// Use this for initialization
-	void Start () {
-        dist = this.gameObject.transform.position.x - follow.transform.position.x;
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    void LateUpdate()
+	void Start ()
     {
-        Vector3 pos = this.gameObject.transform.position;
-        Vector3 tpos = follow.transform.position;
-        this.gameObject.transform.position = new Vector3(tpos.x + dist, pos.y, pos.z);
+        xOffset = transform.position.x - follow.transform.position.x;
+        yPosition = transform.position.y;
+        zPosition = transform.position.z;
+    }
+
+    void LateUpdate ()
+    {   
+        transform.position = new Vector3(follow.transform.position.x + xOffset, yPosition, zPosition);
     }
 }
