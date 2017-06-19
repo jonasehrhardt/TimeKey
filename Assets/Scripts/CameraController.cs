cdf649598ad11265f442da0799850be182fa3773
Gilga <gilgatx@gmail.com>
2017-06-19 12:52:59 +0200

    updated controls (example)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject follow = null;
+    private float dist = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        dist = this.gameObject.transform.position.x - follow.transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void LateUpdate()
+    {
+        Vector3 pos = this.gameObject.transform.position;
+        Vector3 tpos = follow.transform.position;
+        this.gameObject.transform.position = new Vector3(tpos.x + dist, pos.y, pos.z);
+    }
+}
--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -8,10 +8,16 @@ public class CharacterController : MonoBehaviour
     private Vector3 forwardMovement;
 
     public float jumpForce = 1f;
+    public float dashForce = 1f;
     private Rigidbody rigidBody;
-	
-	void Start ()
+
+    private decimal pastTime = 0;
+    Vector3 scale = new Vector3(1f,1f,1f);
+
+    void Start ()
     {
+        scale = this.gameObject.transform.localScale;
+        pastTime = getTime();
         forwardMovement = new Vector3(movementSpeed, 0, 0);
         rigidBody = GetComponent<Rigidbody>();
 	}
@@ -19,14 +25,43 @@ public class CharacterController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetButtonDown("Jump"))
+        Transform trans = this.gameObject.transform;
+
+        if (Input.GetButtonDown("Jump") || (Input.GetButtonDown("Vertical") &&  Input.GetAxisRaw("Vertical") > 0))
         {            
             rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
-	}
+
+        if (Input.GetButtonDown("Horizontal") && Input.GetAxisRaw("Horizontal") > 0)
+        {
+            rigidBody.AddForce(Vector3.right * dashForce, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.RightControl)|| Input.GetKey(KeyCode.C) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) trans.localScale = scale / 2;
+        else if (trans.localScale.y < 1f) trans.localScale = scale;
+    }
+
+    decimal getTime()
+    {
+        return System.DateTime.Now.Ticks / (decimal)System.TimeSpan.TicksPerMillisecond;
+    }
+
+    bool tick(decimal millisec)
+    {
+        decimal current = getTime();
+        if ((current - pastTime) >= millisec)
+        {
+            pastTime = getTime();
+            return true;
+        }
+        return false;
+    }
 
     private void FixedUpdate()
     {
-        transform.Translate(forwardMovement);
+        if (!Input.GetKey(KeyCode.A)) {
+            if (rigidBody.velocity.x > 0 && rigidBody.velocity.x <= 5) rigidBody.AddForce(Vector3.right * 1f, ForceMode.Impulse);
+            else rigidBody.AddForce(Vector3.right * 0.1f, ForceMode.Impulse);
+        }
     }
 }
