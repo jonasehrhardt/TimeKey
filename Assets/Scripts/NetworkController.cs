2c2ec26cb6b517a2b701535f40b139adb91f0b3e
Christoph Holtmann <christoph.holtmann@live.de>
2017-06-21 18:33:49 +0200

    Some network experiments - need to be fixed

--- Assets/Scripts/NetworkController.cs
+++ Assets/Scripts/NetworkController.cs
@@ -2,11 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(NetworkManager))]
 public class NetworkController : MonoBehaviour
 {
     public PasswordKeyGenerator passwordKeyGenerator;
+
+    public GameObject ConnectionUI;
+    public GameObject GameUI;
+
+    public Text PasswordText;
+
     private NetworkManager manager;
 
     void Awake()
@@ -20,15 +27,44 @@ public class NetworkController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        GameUI.SetActive(false);
 
+        manager.StartMatchMaker();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.C))
+        
+
+        //MasterServer.RequestHostList("TimeKeyGame");
+        //MasterServer.dedicatedServer = true;
+
+        //Debug.Log(manager.matchMaker.ListMatches(0, 1, "TimeKeyGame", false, 0, 0, null));
+
+        if (!Network.isServer && !Network.isClient  /*&& MasterServer.PollHostList().Length == 0 */ && Input.GetKeyDown(KeyCode.C))
         {
-            manager.matchMaker.CreateMatch(manager.matchName, 3, true, passwordKeyGenerator.PasswordKey.ToString(), "", "", 0, 0, manager.OnMatchCreate);
+            ConnectionConfig conf = new ConnectionConfig();
+            conf.
+            manager.StartServer();
+            //manager.matchMaker.CreateMatch("TimeKeyGame", 3, true, passwordKeyGenerator.PasswordKey.ToString(), "", "", 0, 0, null);
+            //Network.InitializeServer(2, 7777, !Network.HavePublicAddress());
+            //MasterServer.RegisterHost("TimeKeyGame", "TimeKey");
+
+            ConnectionUI.SetActive(false);
+            GameUI.SetActive(true);
+        }
+
+        if (Network.isServer)
+            Network.incomingPassword = passwordKeyGenerator.PasswordKey.ToString();
+        else if (!Network.isClient && MasterServer.PollHostList().Length > 0 && Input.GetKeyDown(KeyCode.C))
+        {
+            //var networkError = Network.Connect(MasterServer.PollHostList()[0], PasswordText.text);
+
+            manager.StartClient();
+
+            //Debug.Log(networkError);
+            //manager.matchMaker.CreateMatch(manager.matchName, 3, true, passwordKeyGenerator.PasswordKey.ToString(), "", "", 0, 0, manager.OnMatchCreate
         }
     }
 
@@ -39,7 +75,18 @@ public class NetworkController : MonoBehaviour
         if (!manager.IsClientConnected() && !NetworkServer.active && manager.matchMaker == null)
         {
             manager.matchMaker.JoinMatch(new UnityEngine.Networking.Types.NetworkID(), passwordKey, "", "", 0, 0, manager.OnMatchJoined);
-            //manager.StartClient();
         }
+    }
+
+    private void OnConnectedToServer()
+    {
+        ConnectionUI.SetActive(false);
+        GameUI.SetActive(true);
+    }
+
+    private void OnDisconnectedFromServer(NetworkDisconnection info)
+    {
+        GameUI.SetActive(false);
+        ConnectionUI.SetActive(true);
     }
 }
