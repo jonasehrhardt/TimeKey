365a96b5c42e8e27931aefc63ac370ac3079514a
Gilga <gilgatx@gmail.com>
2017-07-10 14:03:14 +0200

    removed unnecessary playerscript variable

--- Assets/Scripts/SlowMoField.cs
+++ Assets/Scripts/SlowMoField.cs
@@ -2,25 +2,14 @@
 
 public class SlowMoField : MonoBehaviour
 {
-    static private PassiveCharacterController characterScript = null;
     public InputManager.SingleInputType Type1;
     public InputManager.SingleInputType Type2;
-
-    void Start()
-    {
-        if (characterScript == null)
-        {
-            GameObject character = GameObject.Find("Character");
-            characterScript = (PassiveCharacterController)character.GetComponent(typeof(PassiveCharacterController));
-        }
-    }
-
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
-            characterScript.TriggerEnter(Type1, Type2);
+            GameManager.instance.pcharacter.TriggerEnter(Type1, Type2);
         }
     }
 
@@ -28,7 +17,7 @@ public class SlowMoField : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            characterScript.TriggerExit(Type1, Type2);
+            GameManager.instance.pcharacter.TriggerExit(Type1, Type2);
         }
     }
 }
