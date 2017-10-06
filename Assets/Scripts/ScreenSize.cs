3e4641d6cb94ecd0dbff4fc8b2104c249b47a043
Jonas <jonas.ehrhardt@student.htw-berlin.de>
2017-10-06 19:29:14 +0200

    script for forcing screen size attached to networkmanager

---
+++ Assets/Scripts/ScreenSize.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ScreenSize : MonoBehaviour {
+
+    public int width = 300;
+    public int height = 100;
+    public bool fullscreen = false;
+
+    private void Awake()
+    {
+        Screen.SetResolution(width, height, fullscreen);
+    }
+}
