4d68927e0d950cad9bb4e0134462337bb9f79c5a
Jonas <jonas.ehrhardt@student.htw-berlin.de>
2017-10-08 00:09:31 +0200

    press 'O' and 'P' to increase/decrease window scale

--- Assets/Scripts/ScreenSize.cs
+++ Assets/Scripts/ScreenSize.cs
@@ -4,10 +4,31 @@ public class ScreenSize : MonoBehaviour {
 
     public int width = 300;
     public int height = 100;
-    public bool fullscreen = false;
+    
+    private int currentScaleFactor = 1;
 
     private void Awake()
     {
-        Screen.SetResolution(width, height, fullscreen);
+        Screen.SetResolution(width, height, false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (currentScaleFactor < 5)
+            {
+                currentScaleFactor += 1;
+                Screen.SetResolution(width * currentScaleFactor, height * currentScaleFactor, false);
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (currentScaleFactor > 1)
+            {
+                currentScaleFactor -= 1;
+                Screen.SetResolution(width * currentScaleFactor, height * currentScaleFactor, false);
+            }
+        }
     }
 }
