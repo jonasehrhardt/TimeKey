644a585d7a39f4ce9a6a13acc19b39e5ee0355cf
Unknown <christoph.holtmann@live.de>
2017-10-10 23:25:26 +0200

    fix end of to tutorial and start endless game

---
+++ Assets/Scripts/LoadEndlessGameTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadEndlessGameTrigger : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            SceneManager.UnloadSceneAsync(2);
+            SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
+        }
+    }
+}
